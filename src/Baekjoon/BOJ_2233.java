55accce6454569cac2f7e8950f02ba3da9524835
KimYongJun <rladydwns129@naver.com>
2023-03-02 20:35:18 +0900

    BOJ_2233 풀이 코드

---
+++ src/Baekjoon/BOJ_2233.java
@@ -0,0 +1,75 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ_2233 {
+    static int n;
+    static int[] parent;
+    static int[] depth;
+    static int[] match;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        parent = new int[n + 1];
+        depth = new int[n + 1];
+        match = new int[2 * n + 1];
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        String s = br.readLine();
+        for (int i = 1, idx = 1; i <= 2 * n; i++) {
+            if (s.charAt(i - 1) == '0') {
+                parent[idx] = stack.peek();
+                match[i] = idx;
+                depth[idx] = stack.size();
+                stack.push(idx++);
+            } else {
+                match[i] = stack.peek();
+                stack.pop();
+            }
+        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        findLCA(match[x], match[y]);
+    }
+
+    private static void findLCA(int x, int y) {
+        int xDepth = depth[x];
+        int yDepth = depth[y];
+
+        while (xDepth > yDepth) {
+            x = parent[x];
+            xDepth--;
+        }
+
+        while (xDepth < yDepth) {
+            y = parent[y];
+            yDepth--;
+        }
+
+        while (x != y) {
+            x = parent[x];
+            y = parent[y];
+        }
+
+        int[] ans = new int[2];
+
+        for (int i = 1, cnt = 0; i <= 2 * n; i++) {
+            if (match[i] == x) {
+                ans[cnt++] = i;
+            }
+            if (cnt == 2) break;
+        }
+
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+}
