72fd6ddef061f2b8aeb88bf3e39e7c4c39ee7ac3
KimYongJun <rladydwns129@naver.com>
2023-03-03 21:32:45 +0900

    BOJ_1594 풀이 코드

--- src/Baekjoon/BOJ_1594.java
+++ src/Baekjoon/BOJ_1594.java
@@ -5,49 +5,51 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ_1594 {
-    static int[][] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] number = br.readLine().toCharArray();
+        String s = br.readLine();
+        int n = s.length();
 
-        if (number.length == 2) {
-            System.out.println(number[0] + "" + number[1]);
+        if (n == 2) {
+            System.out.println(s);
         } else {
-            int n = number.length;
-            dp = new int[n + 1][2];
+            int[][] dp = new int[n + 1][2];
             String[] maxScorePhoneNum = new String[n + 1];
 
-            maxScorePhoneNum[2] = number[0] + "" + number[1];
-            maxScorePhoneNum[3] = maxScorePhoneNum[2] + number[2];
+            maxScorePhoneNum[2] = s.substring(0, 2);
+            maxScorePhoneNum[3] = s.substring(0, 3);
 
             dp[1][0] = dp[1][1] = -5000;
-            dp[2][0] = (number[0] == number[1]) ? 2 : 0;
-            if (number[0] == number[1] || number[0] == number[2] || number[1] == number[2]) {
-                if (number[0] == number[1] && number[1] == number[2]) dp[3][1] = 2;
-                else dp[3][1] = 1;
-            }
 
-            for (int i = 4; i <= n; i++) {
+            for (int i = 2; i <= n; i++) {
                 dp[i][0] = Math.max(dp[i - 2][0], dp[i - 2][1]);
-                dp[i][0] += (number[i - 2] == number[i - 1]) ? 2 : 0;
+                char c1 = s.charAt(i - 2);
+                char c2 = s.charAt(i - 1);
+                dp[i][0] += (c1 == c2) ? 2 : 0;
 
+                if (i == 2) continue;
                 dp[i][1] = Math.max(dp[i - 3][0], dp[i - 3][1]);
-                if (number[i - 3] == number[i - 2] || number[i - 3] == number[i - 1] || number[i - 2] == number[i - 1]) {
-                    if (number[i - 3] == number[i - 2] && number[i - 2] == number[i - 1]) dp[i][1] += 2;
-                    else dp[i][1] += 1;
+                char c3 = s.charAt(i - 3);
+                if (c1 == c2 || c2 == c3 || c1 == c3) {
+                    if (c1 == c2 && c2 == c3) dp[i][1]++;
+                    dp[i][1]++;
                 }
 
-                if (dp[i][0] > dp[i][1]) {
-                    maxScorePhoneNum[i] = maxScorePhoneNum[i - 2] + '-' + number[i - 2] + number[i - 1];
-                } else if (dp[i][0] < dp[i][1]) {
-                    maxScorePhoneNum[i] = maxScorePhoneNum[i - 3] + '-' + number[i - 3] + number[i - 2] + number[i - 1];
-                } else {
-                    String s1 = maxScorePhoneNum[i - 2] + '-' + number[i - 2] + number[i - 1];
-                    String s2 = maxScorePhoneNum[i - 3] + '-' + number[i - 3] + number[i - 2] + number[i - 1];
+                if (i < 4) continue;
 
-                    if (s1.compareTo(s2) <= 0) {
-                        maxScorePhoneNum[i] = s1;
-                    } else maxScorePhoneNum[i] = s2;
+                String s1 = maxScorePhoneNum[i - 2] + '-' + s.substring(i - 2, i);
+                if (dp[i][0] > dp[i][1]) {
+                    maxScorePhoneNum[i] = s1;
+                } else {
+                    String s2 = maxScorePhoneNum[i - 3] + '-' + s.substring(i - 3, i);
+                    if (dp[i][0] < dp[i][1]) {
+                        maxScorePhoneNum[i] = s2;
+                    } else {
+                        if (s1.compareTo(s2) <= 0) {
+                            maxScorePhoneNum[i] = s1;
+                        } else maxScorePhoneNum[i] = s2;
+                    }
                 }
             }
 
