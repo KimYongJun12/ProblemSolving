77fafb8b99bd8ec67d4e4ba16f30ea41409835f0
KimYongJun <rladydwns129@naver.com>
2023-05-20 21:53:36 +0900

    프로그래머스 181188 요격 시스템 풀이 코드

---
+++ src/Programmers/BaseFrame.java
@@ -0,0 +1,21 @@
+package Programmers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaseFrame {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Solution s = new Solution();
+
+
+    }
+}
+
+class Solution {
+    public void solution() {
+
+    }
+}
+
