754877c1940cd12a19b960e5071b380cb0e86cd5
KimYongJun <rladydwns129@naver.com>
2023-05-20 21:49:27 +0900

    프로그래머스 181188 요격 시스템 풀이 코드

--- src/Programmers/Prog_181188/Solution.java
+++ src/Programmers/Prog_181188/Solution.java
@@ -1,30 +1,5 @@
 package Programmers.Prog_181188;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
-public class Prog_181188 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        Solution s = new Solution();
-        int N = Integer.parseInt(br.readLine());
-        int[][] targets = new int[N][2];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            targets[i][0] = a;
-            targets[i][1] = b;
-        }
-
-        System.out.println(s.solution(targets));
-    }
-}
+import java.util.*;
 
 class Solution {
     static class Range implements Comparable<Range> {
@@ -64,3 +39,4 @@ class Solution {
         return answer;
     }
 }
+
