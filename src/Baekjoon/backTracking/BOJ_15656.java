3307727ef8796f374d109c903aba667edb429749
KimYongJun <rladydwns129@naver.com>
2023-06-11 10:58:29 +0900

    BOJ_15655 풀이 코드 BOJ_15656 풀이 코드

---
+++ src/Baekjoon/backTracking/BOJ_15656.java
@@ -0,0 +1,47 @@
+package Baekjoon.backTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_15656 {
+    static int N, M;
+    static int[] num;
+    static int[] ans;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        num = new int[N];
+        ans = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+        backTracking(0);
+        System.out.println(sb);
+    }
+
+    private static void backTracking(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(ans[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            ans[depth] = num[i];
+            backTracking(depth + 1);
+        }
+    }
+}
