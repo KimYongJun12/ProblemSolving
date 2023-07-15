77d00d77692f7bea80aabe6a63c18f02cbb8c6fd
KimYongJun <rladydwns129@naver.com>
2023-07-15 18:26:33 +0900

    BOJ_10810 풀이 코드

--- src/Baekjoon/implementation/BOJ_10810.java
+++ src/Baekjoon/implementation/BOJ_10810.java
@@ -1,11 +1,11 @@
-package Baekjoon;
+package Baekjoon.implementation;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ_10810 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
