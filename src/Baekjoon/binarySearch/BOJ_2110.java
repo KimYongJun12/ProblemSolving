a1178a5f153a64dc662f67cc6154da45dcbd8b42
KimYongJun <rladydwns129@naver.com>
2023-07-16 16:00:01 +0900

    BOJ_2110 풀이 코드

--- src/Baekjoon/binarySearch/BOJ_2110.java
+++ src/Baekjoon/binarySearch/BOJ_2110.java
@@ -7,44 +7,42 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_2110 {
-    static int N, C;
     static int[] house;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
         house = new int[N];
-
         for (int i = 0; i < N; i++) {
             house[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(house);
 
-        int lo = 1, hi = house[N - 1] - house[0];
+        int lo = 0, hi = house[N - 1] - house[0] + 1;
+
+        // upper bound
         while (lo < hi) {
             int mid = (lo + hi) / 2;
-            if (binarySearch(mid)) {
-                hi = mid;
-            } else lo = mid + 1;
+            if (binarySearch(mid) >= C) {
+                lo = mid + 1;
+            } else hi = mid;
         }
 
-        System.out.println(hi - 1);
+        System.out.println(lo - 1);
     }
 
-    private static boolean binarySearch(int distance) {
+    private static int binarySearch(int value) {
         int cnt = 1;
         int last = house[0];
 
-        for (int i = 1; i < N; i++) {
-            int now = house[i];
-            if (now - last >= distance) {
+        for (int i = 1; i < house.length; i++) {
+            if (house[i] - last >= value) {
                 cnt++;
-                last = now;
+                last = house[i];
             }
         }
-
-        return cnt < C;
+        return cnt;
     }
 }
