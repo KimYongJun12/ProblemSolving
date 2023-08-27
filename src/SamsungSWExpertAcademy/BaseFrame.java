e96bb6d8ea76920967610aa6bf17658fd29348fe
김용준 <rladydwns129@naver.com>
2023-08-27 20:32:42 +0900

    SWEA_1230 풀이 코드

--- src/SamsungSWExpertAcademy/BaseFrame.java
+++ src/SamsungSWExpertAcademy/BaseFrame.java
@@ -5,7 +5,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/**
+ * 풀이 시작 :
+ * 풀이 완료 :
+ * 풀이 시간 :
+ *
+ * 문제 해석
+ *
+ * 구해야 하는 것
+ *
+ * 문제 입력
+ *
+ * 제한 요소
+ *
+ * 생각나는 풀이
+ *
+ *
+ * 구현해야 하는 기능
+ *
+ */
 class BaseFrame {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,5 +38,6 @@ class BaseFrame {
 
             sb.append('#').append(test_case).append(' ').append("").append('\n');
         }
+        System.out.println(sb);
     }
 }
